ea1ee93ca043cc6cac4ea9b7ed6527fad76a2434
dan <dan@danwritescode.com>
2020-06-12 11:01:35 +0800

    Updates

---
+++ precache-manifest.8c5c74dfa2d76b415fb41104d180af30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455a209fc5868025b311e243982e2ef0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cbc9223f4c76e747ab74",
+    "url": "/static/css/main.7c9b92ec.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc9223f4c76e747ab74",
+    "url": "/static/js/main.a772114f.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
